a25038869b3ab00c48053762906abe3e45a2ede8
Sahir <sahirc@gmail.com>
2015-02-19 19:48:08 -0800

    I think callWithHandler works

--- InterpCont.hs
+++ InterpCont.hs
@@ -5,7 +5,7 @@ import Result
 import SExp
 import Token
 
--- Values resulting from interpreting an expression.
+-- Values resulting from parseCheckAndInterpStreting an expression.
 data Val = NumV Integer -- a numeric constant
          | BoolV Bool -- a boolean constant
          | StringV String -- a string constant
@@ -31,10 +31,13 @@ data Cont = DoneK
           | IfK CExpr CExpr Env Cont
           | AppFunK CExpr Env Cont
           | AppArgK Val Cont
+          | CustomK Val Cont
           deriving Show
 
 handleError :: Cont -> Val -> Result Val
-handleError _ = \val -> Err ("unhandled error: " ++ (show val))
+handleError k val = case k of 
+                      AppArgK _ b  -> case b of 
+                                        CustomK handler cont -> apply handler val cont
 
 wrapBinaryArithOp :: String -> (Integer -> Integer -> Val) -> Val
 wrapBinaryArithOp name op =
@@ -72,7 +75,7 @@ cons = PrimV "cons" (\l k -> callK k (PrimV ("partial:" ++ "cons")
                                       )
                     )
 
-first = PrimV "first" (\list k -> case list of
+first = PrimV "first" (\list k -> case list of 
                                     ConsV l r -> callK k l
                                     _ -> handleError k (StringV "Can't first a nonList"))
 
@@ -85,14 +88,20 @@ pair = PrimV "pair" (\l k -> callK k (PrimV ("partial:" ++ "cons")
                                         )))
 pairFst = PrimV "fst" (\p k -> case p of 
                                   PairV l _ -> callK k l
-                                  _ -> handleError k (StringV "Can get snd of a nonPair"))
+                                  _ -> handleError k (StringV "Can't get snd of a nonPair"))
                                         
 pairSnd = PrimV "snd" (\p k -> case p of 
                                   PairV _ r -> callK k r
-                                  _ -> handleError k (StringV "Can get snd of a nonPair"))
+                                  _ -> handleError k (StringV "Can't get snd of a nonPair"))
 
 raise = PrimV "raise" (\arg k -> handleError k arg)
-callWithHandler = unimplemented "call-with-handler"
+
+callWithHandler = PrimV "call-with-handler" (\thunk k -> callK k (PrimV "handler" (
+                                              \handler k -> apply thunk EmptyV (
+                                                (CustomK handler k)))))
+                                                              
+
+
 callWithContext = unimplemented "call-with-context"
 getContext = unimplemented "get-context"
 callCc = unimplemented "call/cc"
@@ -133,11 +142,8 @@ callK k val =
       BoolV False -> interp alt env k
       nonBool -> Err ("`if` expected bool, got: " ++ (show nonBool))
    AppFunK arg env k -> interp arg env (AppArgK val k)
-   AppArgK arg' k ->  apply arg' val k
-
-                      {-case arg' of 
-                       FunV var body env' -> interp body ((var, val):env') k
-                       PrimV name f -> f val k-}
+   AppArgK arg k ->  apply arg val k
+   CustomK _ cont -> callK cont val
 
 parseCheckAndInterpStr :: String -> Result Val
 parseCheckAndInterpStr str =
@@ -152,4 +158,5 @@ parseCheckAndInterpStr str =
 apply :: Val -> Val -> Cont -> Result Val
 apply (FunV var cexpr env) val k = interp cexpr ((var, val):env) k
 apply (PrimV name f) arg k = f arg k
+
 apply _ _ _ = fail "Not a FunV or PrimV"
